136edd501da8540342fb14f4756ff683311a6c79
Angel Simeonov <ass540@its.york.ac.uk>
2020-03-01 17:32:21 +0000

    second layer for replica patterns added

--- INNS/assessment/assessment.m
+++ INNS/assessment/assessment.m
@@ -1,39 +1,72 @@
-% N = [1,2,3,4,5,10,15,20,25];
+N = [1,2,3,4,5,10,15,20,25];
+L = N;
 % N = [1:1:100];
 %N = [10];
 %transpose for matlab
-%Xm = X{:,:}.';
+Xm = X{:,:}.';
 % Xm = X_SisPorto.';
- Xm = X{:,:}(:,[20,2,21,5]).'; % first 4 important features
+%Xm = X{:,:}(:,[20,2,21,5]).'; % first 4 important features
 Ym = ohY.';
 test_CE = zeros(1, length(N)); % vector of all the cross-entropy values against the test set
 test_conf = zeros(1, length(N)); % vector of all the fractions of samples missclasified in the test set
 best_model = struct('neur', NaN, 'CE', Inf, 'net', NaN, 'Y', NaN, 'predY', NaN);
+
+%single hidden layer
+% for n = 1:length(N)
+%     net = patternnet(N(n));
+%     [net, tr] = train(net, Xm, Ym);
+%     
+%     %extract only the entries used for test
+%     Xtest = Xm(:,tr.testInd);
+%     Ytest = Ym(:,tr.testInd);
+%     
+%     predY = net(Xtest);
+%     %evaluating against testperf because it is the only unbiased dataset
+%     %todo, use the same extraction and get the confusion matrix values for
+%     %that to compare alongside the Cross entropy
+%     test_CE(n) = perform(net, Ytest, predY);
+%     test_conf(n) = confusion(Ytest, predY);
+%     
+%     if test_CE(n) < best_model.CE
+%         best_model.CE = test_CE(n);
+%         best_model.net = net;
+%         best_model.Y = Ytest;
+%         best_model.predY = predY;
+%         best_model.neur = n;
+%     end 
+% end
+
+%multiple hidden layers
 for n = 1:length(N)
-    net = patternnet(N(n));
-    [net, tr] = train(net, Xm, Ym);
-    
-    %extract only the entries used for test
-    Xtest = Xm(:,tr.testInd);
-    Ytest = Ym(:,tr.testInd);
-    
-    predY = net(Xtest);
-    %evaluating against testperf because it is the only unbiased dataset
-    %todo, use the same extraction and get the confusion matrix values for
-    %that to compare alongside the Cross entropy
-    test_CE(n) = perform(net, Ytest, predY);
-    test_conf(n) = confusion(Ytest, predY);
-    
-    if test_CE(n) < best_model.CE
-        best_model.CE = test_CE(n);
-        best_model.net = net;
-        best_model.Y = Ytest;
-        best_model.predY = predY;
-        best_model.neur = n;
-    end 
+    idL = L < N(n); % subsequent hl must always have less neurons than the preceding
+    HL = L(idL);
+    for l = 1:length(HL)
+        
+        net = patternnet([N(n),HL(l)]);
+        [net, tr] = train(net, Xm, Ym);
+
+        %extract only the entries used for test
+        Xtest = Xm(:,tr.testInd);
+        Ytest = Ym(:,tr.testInd);
+
+        predY = net(Xtest);
+        %evaluating against testperf because it is the only unbiased dataset
+        %todo, use the same extraction and get the confusion matrix values for
+        %that to compare alongside the Cross entropy
+        test_CE(n) = perform(net, Ytest, predY);
+        test_conf(n) = confusion(Ytest, predY);
+
+        if test_CE(n) < best_model.CE
+            best_model.CE = test_CE(n);
+            best_model.net = net;
+            best_model.Y = Ytest;
+            best_model.predY = predY;
+            best_model.neur = n;
+        end 
+    end
 end
 
-saveFigs('4_important','fig', N, test_CE, test_conf, best_model, true);
+saveFigs('two_layer','fig', N, test_CE, test_conf, best_model, true);
 
 function saveFigs(nameX, filetype, N, test_CE, test_conf, best_model, isTest)
   if isTest
@@ -59,7 +92,7 @@ function saveFigs(nameX, filetype, N, test_CE, test_conf, best_model, isTest)
   saveas(gcf,fullfile(path, 'best_conf_matrix'), filetype);
   
   jframe = view(best_model.net);
-  saveJframe(jframe, fullfile(path, 'net_architecture'));
+  saveJframe(jframe, fullfile(path, 'best_architecture'));
   
   save(fullfile(path,'best_model'), 'best_model');
 end
