a584a4cfb6727b8955894c64da73340d77ae5039
Angel <angel.st.sim@gmail.com>
2020-03-19 22:19:46 +0200

    vb vs mcmc +forward/backward prob .txts +refactoring

--- MLPG/assessment/q4/q4.py
+++ MLPG/assessment/q4/q4.py
@@ -1,9 +1,6 @@
-# transitions = {"From":[], "To":[], "Prob":[]}
-# emmisions = {"State":[], "Symbol":[], "Prob":[]}
 transitions = {}
 emmisions = {}
 observations = [[]]
-# states = ('(0,0)','(0,1)','(0,2)','(0,3)','(1,0)','(1,1)','(1,2)','(1,3)','(2,1)','(2,3)','(3,0)','(3,1)','(3,2)','(3,3)')
 states = ('(0,0)','(0,1)','(1,0)','(1,1)')
 def main():
     with open('q4hmm.txt') as f:
@@ -13,12 +10,12 @@ def main():
             i += 1
             line = line.rstrip().split(' ')
             if i > 2 and i <= 22:
-                # Do Transition
+                # load Transition
                 if (line[0] not in transitions.keys()):
                     transitions[line[0]] = {}
                 transitions[line[0]].update({line[1]: float(line[2])})
             elif i >= 25:
-                # Do Emmision
+                # load Emmision
                 if (line[0] not in emmisions.keys()):
                     emmisions[line[0]] = {}
                 emmisions[line[0]].update({line[1]: float(line[2])})
@@ -31,26 +28,17 @@ def main():
                 observations.append([])
             else:
                 observations[-1].append(line)
+        observations.pop()
 
-    print(transitions)
-    print("="*20)
-    print("-"*20)
-    print("="*20)
-    print(emmisions)
-    #print prob for a given state transition
-    print(sum(transitions['(None,None)'].values()))
-
-    # test
-    # test = observations[0]
-    # print(test)
-    # print(test[1:])
-    # print(reversed(test[1:]))
-    # print(reversed(test[1:]+(None,)))
+    print(observations)
 
     print(" FORWARD PROBABILITIES ")
-    print_prob(0,len(observations[0]),forward(observations[0]))
+    for i, obsrv in enumerate(observations):
+        print_prob(i,len(obsrv),forward(obsrv))
+    
     print(" BACKWARD PROBABILITIES ")
-    print_prob(0,len(observations[0]), backward(observations[0]))
+    for i, obsrv in enumerate(observations):
+        print_prob(i,len(obsrv), backward(obsrv))
 
 def forward(observ):
     forward_prob = []
@@ -59,7 +47,6 @@ def forward(observ):
         h_c = {}
         for state in states:
             if i == 0:
-                print("Starting state...")
                 total_h = transitions['(None,None)'][state]
             else:
                 total_h = sum(h_prev[j] * transitions[j][state] for j in states)
@@ -67,8 +54,7 @@ def forward(observ):
         forward_prob.append(h_c)
         h_prev = h_c
     return forward_prob
-# uses transition prob and emission prob
-# beta for the last time point is == 1
+
 def backward(obsrv):
     backward_prob = []
     h_prev = {}
@@ -90,13 +76,7 @@ def print_prob(n_seq, length, probabilities):
         for prob in probabilities: 
             line += ' {:.3f}'.format(prob[state])
         print(line)
-
-    
-
-
-
-def tuple_converter(s):
-    return tuple(int(i) for i in s[1:-1].split(','))
+    print()
 
 if __name__ == '__main__':
     main()
